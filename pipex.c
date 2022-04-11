1010ca81c5f83d60195a85e3e876916770f290fe
ufitzhug <ufitzhug@ge-f4.kzn.21-school.ru>
2022-04-11 23:58:21 +0300

    11 April 2022 11:50 PM

--- pipex.c
+++ pipex.c
@@ -14,11 +14,17 @@
 
 void	ft_execute_func(char *argv, char **envp, t_list *pipex)
 {
+	if (!argv[0])
+	{
+		pipex->args_command[0] = ft_strdup("cat");
+		pipex->args_command[1] = NULL;
+	}
+	else
+		pipex->args_command = ft_split(argv, ' ');
 	pipex->path = ft_find_path(envp);
 	pipex->path_command = ft_split(pipex->path, ':');
-	pipex->args_command = ft_split(argv, ' ');
 	pipex->command = ft_check_access(pipex->path_command,
-			pipex->args_command[0]);
+									 pipex->args_command[0]);
 	if (!pipex->command)
 	{
 		ft_free_child(pipex);
@@ -26,6 +32,7 @@ void	ft_execute_func(char *argv, char **envp, t_list *pipex)
 		exit(1);
 	}
 	execve(pipex->command, pipex->args_command, envp);
+
 }
 
 void	ft_last_exec(char *argv, char **envp, t_list *pipex)
@@ -45,6 +52,7 @@ void	ft_close_p(t_list *pipex)
 
 void	ft_pipex(char *argv, char **envp, t_list *pipex)
 {
+	waitpid(-1, NULL, 0);
 	if (pipe(pipex->tube) < 0)
 		ft_error("Pipe function error");
 	pipex->pid = fork();
@@ -56,7 +64,7 @@ void	ft_pipex(char *argv, char **envp, t_list *pipex)
 			ft_error("CLOSE PIPE ERROR");
 		if (dup2(pipex->tube[0], 0) == -1)
 			ft_error("DUP2 ERROR");
-		waitpid(0, NULL, 0);
+//		waitpid(0, NULL, 0);
 	}
 	else
 	{
@@ -64,7 +72,9 @@ void	ft_pipex(char *argv, char **envp, t_list *pipex)
 			ft_error("CLOSE PIPE ERROR");
 		if (dup2(pipex->tube[1], 1) == -1)
 			ft_error("DUP2 ERROR");
+		waitpid(0, NULL, 0);
 		ft_execute_func(argv, envp, pipex);
+
 	}
 }
 
